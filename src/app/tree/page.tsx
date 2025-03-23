ba92b3dce312414f000ecc54e0f13eed09c22f1f
Manish Yadav <01devmanish@gmail.com>
2025-03-23 10:50:55 +0530

    Update page.tsx

--- src/app/tree/page.tsx
+++ src/app/tree/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useState } from "react";
-import TreeVisualizer from "../../components/TreeVisualizer";
-import CodeDisplay from "../../components/CodeDisplay";
+import TreeVisualizer from "../../components/tree/TreeVisualizer";
+import CodeDisplay from "../../components/tree/CodeDisplay";
 
 const TreePage: React.FC = () => {
   const [code, setCode] = useState<string>("");
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6 flex flex-col items-center">
+    <div className="h-screen w-screen bg-gray-100 p-6 flex flex-col items-center overflow-auto ">
       <div className="flex w-full max-w-6xl gap-6">
         <div className="w-2/3">
           <TreeVisualizer setCode={setCode} />
